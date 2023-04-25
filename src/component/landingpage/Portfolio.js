06f326fb2daf76f7a95b081cbd808cb3d7fbbc35
David Alfian <davealfian72@gmail.com>
2023-04-25 11:02:21 +0700

    update card portfolio

--- src/component/landingpage/Portfolio.js
+++ src/component/landingpage/Portfolio.js
@@ -20,31 +20,6 @@ const Portfolio = () => {
                         </div>
                     </div>
                     
-                    <div id="card" className="bg-[#ececec] text-hijau344E41 w-[450px] min-h-[12rem] mb-20 shadow-lg rounded-md overflow-hidden ">
-                        <img src={bannerLentera} alt="" className="w-full h-full object-cover"></img>
-                        <div id="desc" className="flex items-start flex-col bg-white">
-                            <div id="badge" className="flex flex-row pl-5 pt-5 pr-5 gap-2">
-                                <span className="font-bold px-3 py-1 rounded-full text-hijau344E41 bg-gray-100">desain UI/UX</span>
-                                <span className="font-bold px-3 py-1 rounded-full text-hijau344E41 bg-gray-100">website</span>
-                            </div>
-                            <span className="font-semibold text-heading4 pl-5 pt-5 pb-2 text-hijau344E41">E-Commerce penjualan aplikasi</span>
-                            <span className="font-normal text-heading4 pl-5 pb-2 text-hijau344E41">Lentera Teknologi Indonesia</span>
-                            <a href="./portfolio" className="place-self-end pl-5 pt-8 pr-5 pb-5"><span className="font-normal text-hijauA3B18A text-heading4 underline italic">selengkapnya</span></a>    
-                        </div>
-                    </div>
-
-                    <div id="card" className="bg-[#ececec] text-hijau344E41 w-[450px] min-h-[12rem] mb-20 shadow-lg rounded-md overflow-hidden ">
-                        <img src={bannerLentera} alt="" className="w-full h-full object-cover"></img>
-                        <div id="desc" className="flex items-start flex-col bg-white">
-                            <div id="badge" className="flex flex-row pl-5 pt-5 pr-5 gap-2">
-                                <span className="font-bold px-3 py-1 rounded-full text-hijau344E41 bg-gray-100">desain UI/UX</span>
-                                <span className="font-bold px-3 py-1 rounded-full text-hijau344E41 bg-gray-100">website</span>
-                            </div>
-                            <span className="font-semibold text-heading4 pl-5 pt-5 pb-2 text-hijau344E41">E-Commerce penjualan aplikasi</span>
-                            <span className="font-normal text-heading4 pl-5 pb-2 text-hijau344E41">Lentera Teknologi Indonesia</span>
-                            <a href="./portfolio" className="place-self-end pl-5 pt-8 pr-5 pb-5"><span className="font-normal text-hijauA3B18A text-heading4 underline italic">selengkapnya</span></a>    
-                        </div>
-                    </div>
                 </div>
                 
         </div>
